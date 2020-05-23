b054ee78902b3a5b8dfa4488a166558250ea19f1
AndreEbel <62537359+AndreEbel@users.noreply.github.com>
2020-05-23 17:12:56 +0200

    add preprocessor for sub and superscript
    effectivelly convert subscripts and superscripts form markdown (HTML actually) to latex and the pdf

--- jupyter_nbconvert_config.py
+++ jupyter_nbconvert_config.py
@@ -1,10 +1,11 @@
 c = get_config()
 
-# The notebook you want to export
-ntb = 'Untitled.ipynb'
-# Exporting your notebook to pdf
-c.NbConvertApp.notebooks = [ntb]
-c.Exporter.preprocessors = [ 'bibcite2c.BibTexPreprocessor', 'pre_pymarkdown.PyMarkdownPreprocessor' ]
+# It is assumed there is only one notebook in the folder
+c.NbConvertApp.notebooks = ['*.ipynb']
+# Preprocessors 
+c.Exporter.preprocessors = ['bibcite2c.BibTexPreprocessor', 
+                            'pre_pymarkdown.PyMarkdownPreprocessor', 
+                            'pre_subsupmarkdown.PyMarkdownPreprocessor']
 c.NbConvertApp.export_format = 'pdf'
 # Extra parameters to remove the code cells (equivalent to --no-input)
 c.TemplateExporter.exclude_output_prompt = True 
